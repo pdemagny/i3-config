5075d7a1a78072cca60a7d8544a4b7d73a3f69ec
Pierre DEMAGNY <pierre.demagny@citymeo.fr>
2018-05-14 16:27:57 +0200

    fixed save workspace feature

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 if [[ "$EUID" -ne 0 ]]; then echo "this installer should be run as root or with sudo privileges, exiting." && exit 1; fi
 
+apt-get install -y i3 lightdm nitrogen xautolock
+
 for i in scripts/*; do
   bn=$(basename ${i})
   cp -v ${i} /usr/local/bin/ && chmod 755 /usr/local/bin/${bn} && chown root:root /usr/local/bin/${bn}
@@ -8,3 +10,5 @@ done
 
 cp -v configs/config /etc/i3/config && chown root:root /etc/i3/config
 cp -v configs/i3status.conf /etc/i3status.conf && chown root:root /etc/i3status.conf
+
+i3-msg restart
